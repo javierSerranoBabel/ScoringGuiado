a41285655050d42433a07b1aa00d5120f10dba33
Javier Serrano <javier.serrano@babelgroup.com>
2023-03-21 11:05:40 +0100

    Creado modelo basico dirrecion

--- src/main/java/com/babel/ScoringGuiado/Models/Direccion.java
+++ src/main/java/com/babel/ScoringGuiado/Models/Direccion.java
@@ -1,4 +1,16 @@
 package com.babel.ScoringGuiado.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Direccion {
+    private int direccionId;
+    private int tipoVia;
+    private String nombreCalle;
+    private String numero;
+    private String codPostal;
+    private String municipio;
+    private String provinciaCod;
 }
