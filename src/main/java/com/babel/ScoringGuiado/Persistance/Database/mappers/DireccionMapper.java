7a2845503ec1d79ed3959268a4f7647c8138a036
Javier Serrano <javier.serrano@babelgroup.com>
2023-03-21 11:18:43 +0100

    Cambiar jdbcType a varchar

--- src/main/java/com/babel/ScoringGuiado/Persistance/Database/mappers/DireccionMapper.java
+++ src/main/java/com/babel/ScoringGuiado/Persistance/Database/mappers/DireccionMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Options;
 @Mapper
 public interface DireccionMapper {
 
-    @Insert("INSERT INTO DIRECCION (TIPO_VIA_ID, NOMBRE_CALLE, NUM, PISO, PUERTA, ESCALERA, OTRO_DATO, COD_POSTAL, MUNICIPIO, COD_PROVINCIA) VALUES (#{tipoViaId}, #{nombreCalle}, #{numero}, #{piso, jdbcType=CHAR}, null, null, null, #{codPostal}, #{municipio}, #{provinciaCod})")
+    @Insert("INSERT INTO DIRECCION (TIPO_VIA_ID, NOMBRE_CALLE, NUM, PISO, PUERTA, ESCALERA, OTRO_DATO, COD_POSTAL, MUNICIPIO, COD_PROVINCIA) VALUES (#{tipoViaId}, #{nombreCalle}, #{numero}, #{piso, jdbcType=VARCHAR}, null, null, null, #{codPostal}, #{municipio}, #{provinciaCod})")
     @Options(useGeneratedKeys = true, keyProperty = "direccionId", keyColumn="DIRECCION_ID")
     void insertDireccion(Direccion direccion);
 
