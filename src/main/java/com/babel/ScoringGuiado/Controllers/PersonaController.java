b32d8bbab7e03e537f2bc3eced591e1dae3a3d77
Javier Serrano <javier.serrano@babelgroup.com>
2023-03-21 10:03:45 +0100

    Prueba post generico

--- src/main/java/com/babel/ScoringGuiado/Controllers/PersonaController.java
+++ src/main/java/com/babel/ScoringGuiado/Controllers/PersonaController.java
@@ -1,10 +1,13 @@
 package com.babel.ScoringGuiado.Controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonaController {
+
+    @PostMapping("persona")
     ResponseEntity addPersona(){
         return ResponseEntity.ok("Add persona encontrada");
     }
