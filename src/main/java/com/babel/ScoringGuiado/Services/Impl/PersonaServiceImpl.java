e00a7c85a82e1db0963d47ba81174c4c309ce81c
Javier Serrano <javier.serrano@babelgroup.com>
2023-03-21 12:47:52 +0100

    Inserccion de persona mapper + servicio

--- src/main/java/com/babel/ScoringGuiado/Services/Impl/PersonaServiceImpl.java
+++ src/main/java/com/babel/ScoringGuiado/Services/Impl/PersonaServiceImpl.java
@@ -2,20 +2,26 @@ package com.babel.ScoringGuiado.Services.Impl;
 
 import com.babel.ScoringGuiado.Models.Persona;
 import com.babel.ScoringGuiado.Persistance.Database.mappers.DireccionMapper;
+import com.babel.ScoringGuiado.Persistance.Database.mappers.PersonaMapper;
 import com.babel.ScoringGuiado.Services.PersonaService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonaServiceImpl implements PersonaService {
     private DireccionMapper direccionMapper;
+    private PersonaMapper personaMapper;
 
-    public PersonaServiceImpl(DireccionMapper direccionMapper) {
+    public PersonaServiceImpl(PersonaMapper personaMapper,DireccionMapper direccionMapper) {
+        this.personaMapper = personaMapper;
         this.direccionMapper = direccionMapper;
     }
 
     @Override
     public Persona addPersona(Persona persona) {
-        return addPersonaDireccion(persona);
+        persona = addPersonaDireccion(persona);
+        this.personaMapper.insertPersona(persona);
+
+        return persona;
 
     }
 
