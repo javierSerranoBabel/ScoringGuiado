2d4399013ba7f1a2b79fff1fc42162ef1f6d295f
Javier Serrano <javier.serrano@babelgroup.com>
2023-03-21 10:23:33 +0100

    Post de prueba con una persona

--- src/main/java/com/babel/ScoringGuiado/Controllers/PersonaController.java
+++ src/main/java/com/babel/ScoringGuiado/Controllers/PersonaController.java
@@ -1,14 +1,16 @@
 package com.babel.ScoringGuiado.Controllers;
 
+import com.babel.ScoringGuiado.Models.Persona;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonaController {
 
     @PostMapping("persona")
-    ResponseEntity addPersona(){
-        return ResponseEntity.ok("Add persona encontrada");
+    ResponseEntity addPersona(@RequestBody Persona persona){
+        return ResponseEntity.ok(persona);
     }
 }
